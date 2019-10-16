f7852a00e968edbafa5a678cc4f805ac643f2d08
Shaine Ingamells <shaineingamells@Shaines-MacBook-Pro.local>
2019-10-15 23:10:47 -0600

    re-did project

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -21,7 +22,11 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+                <div key={movie.id}>
+                <Link to={`/movies/${movie.id}`}>
+                <MovieDetails key={movie.id} movie={movie} />
+                </Link>
+              </div>
       ))}
     </div>
   );
